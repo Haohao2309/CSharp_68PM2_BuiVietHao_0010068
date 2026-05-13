e5efb3a0d7baa43bc495c8784a8138f41d629330
Hao <hao8btandan@gmail.com>
2026-05-13 20:49:05 +0700

    Chức năng đăng nhập

---
+++ WindowsFormsApp01/WindowsFormsApp01/QLSV.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsFormsApp01
+{
+    partial class QLSV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl01 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl01
+            // 
+            this.lbl01.AutoSize = true;
+            this.lbl01.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.lbl01.ForeColor = System.Drawing.Color.Blue;
+            this.lbl01.Location = new System.Drawing.Point(198, 62);
+            this.lbl01.Name = "lbl01";
+            this.lbl01.Size = new System.Drawing.Size(409, 39);
+            this.lbl01.TabIndex = 0;
+            this.lbl01.Text = "Hệ thống quản lí sinh viên";
+            this.lbl01.Click += new System.EventHandler(this.lbl01_Click);
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(228, 230);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(344, 22);
+            this.txtPass.TabIndex = 1;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(228, 158);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(344, 22);
+            this.txtUser.TabIndex = 1;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(228, 136);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(98, 16);
+            this.lbl1.TabIndex = 2;
+            this.lbl1.Text = "Tên đăng nhập";
+            this.lbl1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(228, 208);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(61, 16);
+            this.lbl2.TabIndex = 3;
+            this.lbl2.Text = "Mật khẩu";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(337, 277);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(123, 55);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Đăng nhập";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // QLSV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.lbl01);
+            this.Name = "QLSV";
+            this.Text = "QLSV";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl01;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
---
+++ WindowsFormsApp01/WindowsFormsApp01/QLSV.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp01
+{
+    public partial class QLSV : Form
+    {
+        public QLSV()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl01_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string userName = txtUser.Text;
+            string pass = txtPass.Text;
+            if(userName == "0010068@st.huce.edu.vn" && pass == "0010068")
+            {
+                MessageBox.Show("Đăng nhập thành công !!!");
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại !!!");
+            }
+        }
+    }
+}
